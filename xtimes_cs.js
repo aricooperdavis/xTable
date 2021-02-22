188f5771247385a63ae3058fc49c3e8e9b33c9e1
Ari Cooper-Davis <cooper-davis.arran@cooper-davis.net>
2021-02-22 11:44:57 +0000

    Bugfix: now works when page is reloaded

--- xtimes_cs.js
+++ xtimes_cs.js
@@ -1,15 +1,11 @@
 // xtimes_cs.js
 // Content script is injected into matched pages
 
-// Get puzzle data if available
-let oApp = wrappedJSObject.oApp;
-let message = null;
-
-if (oApp != null){
-  message = wrappedJSObject.oApp.puzzle.JSON.data;
-}
+console.log("injected");
 
 // Listen for pageAction click and return puzzle
 browser.runtime.onMessage.addListener(request => {
-  return Promise.resolve({data: message});
+  if (wrappedJSObject.oApp != null) {
+    return Promise.resolve({data: wrappedJSObject.oApp.puzzle.JSON.data});
+  }
 });
